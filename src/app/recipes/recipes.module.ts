723c381255162ea0d54145c902e53cb8607d7904
aamirubit <aamirsaleem167@gmail.com>
2017-08-21 13:44:49 +0500

    recipe feature module

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,29 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { DropdownDirective } from './../shared/dropdown.directive';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@NgModule({
+    declarations:[
+       RecipesComponent,
+       RecipeDetailComponent,
+       RecipeListComponent,
+       RecipeItemComponent,
+       RecipeStartComponent,
+       RecipeEditComponent,
+       DropdownDirective
+    ],
+    imports:[
+        CommonModule,
+        ReactiveFormsModule
+    ],
+    providers:[],
+
+})
+
+export class RecipesModule{ }
