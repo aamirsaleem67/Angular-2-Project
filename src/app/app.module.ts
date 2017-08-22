d07ec835e37ef6200c6847099f7b19392860b8e1
aamirubit <aamirsaleem167@gmail.com>
2017-08-22 11:49:41 +0500

    auth module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthModule } from './auth/auth.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { RecipesModule } from './recipes/recipes.module';
@@ -8,7 +9,6 @@ import { RecipeService } from './recipes/recipe.service';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 
@@ -17,23 +17,20 @@ import { ToogleComponentService } from './toogle-component.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignupComponent,
-    SigninComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    AuthModule
   ],
   providers: [ShoppingListService,ToogleComponentService,RecipeService, 
               DataStorageService,AuthService, AuthGuard],
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,20 +1,12 @@
-import { AuthGuard } from './auth/auth-guard.service';
-import { SigninComponent } from './auth/signin/signin.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+
 import { NgModule } from '@angular/core';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipesComponent } from './recipes/recipes.component';
 import { Routes,RouterModule } from '@angular/router';
 
 const appRoutes : Routes=[
     { path: '',redirectTo: 'recipes',pathMatch:'full' },
     
     { path: 'shopping',component: ShoppingListComponent },
-    { path: 'signup',component: SignupComponent},
-    { path: 'signin',component:SigninComponent }
 ];
 @NgModule({
     imports:[
